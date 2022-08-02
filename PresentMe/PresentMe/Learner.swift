4d3c6b6a8eeb61d8c4ed2cfac8f1c01345952074
Gianluca Orpello <g.orpello@gmail.com>
2022-08-02 15:17:43 +0200

    Update repository

---
+++ PresentMe/PresentMe/Learner.swift
@@ -0,0 +1,19 @@
+//
+//  Student.swift
+//  DataModelPresentMe
+//
+//  Created by Gianluca Orpello, Luca Palmese and Pasquale Vittoriosi for the Developer Academy on 13/10/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct Learner {
+    
+    var name: String
+    var surname: String
+    var age: Int
+    var imageName: String
+    var shortBio: String
+    
+}
---
+++ docs/CHANGELOG.md
@@ -0,0 +1,13 @@
+## CHANGELOG
+
+## Version 1.0.0 (2021-10-20)
+
+First release:
+
+* PresentMe - Data Modeling Basics
+
+## Version 2.0.0 (2022-08-02)
+
+First release:
+
+* Data Model - Update Repo with new Structure
